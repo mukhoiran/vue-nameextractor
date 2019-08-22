c9ff1b3957f7785f8dd303deb79e5c777c0f369a
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-22 17:29:56 +0700

    add bootstrap style

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
 Vue.component('extractor',{
-  template: "<div><h1>Name Extractor</h1><p>Name extractor is program for extract name automatically</p><div><input type='text' v-model='name' place='Typing your Fullname' @keyup.enter='process'><button @click='process'>process</button></div><h2 v-if='result'>Welcome Mr. / Mrs. {{last | capitalize}}, Happy you can join</h2><b v-if='result'>Result of your extract name :</b><div v-if='result' <p>first name: {{first}}</p><p>middle name: {{middle}}</p><p>last name: {{last}}</p></div></div>",
+  template: "<div class='container'><h1 class='text-center'>Name Extractor</h1><p class='text-center'>Name extractor is program for extract name automatically</p><div class='input-group'><input type='text' v-model='name' place='Typing your Fullname' @keyup.enter='process' class='form-control'><button @click='process' class='input-group-addon'>process</button></div><h2 v-if='result' class='alert alert-success m-2 text-center'>Welcome Mr. / Mrs. {{last | capitalize}}, Happy you can join</h2><b v-if='result' class='text-warning badge badge-dark'>Result of your extract name :</b><div v-if='result' class='rounded border border-danger'> <p>first name: {{first}}</p><p>middle name: {{middle}}</p><p>last name: {{last}}</p></div></div>",
   data: function(){
     return {
       name: '',
